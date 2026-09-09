865bd3a788527c8b726138de9991794b00970651
Zion Opaaje <zopaaje8@gmail.com>
2026-09-09 22:51:42 +0100

    feat: add make available action for coming soon products

--- src/modules/launch-campaigns/launch-campaigns.routes.ts
+++ src/modules/launch-campaigns/launch-campaigns.routes.ts
@@ -8,5 +8,6 @@ const schema=z.object({productId:z.string().min(1),launchAt:z.string().datetime(
 launchCampaignsRouter.use(requireAuth);
 launchCampaignsRouter.get("/mine",async(req,res,next)=>{try{res.json({campaigns:await service.getMine(req.user!.sub)});}catch(e){next(e);}});
 launchCampaignsRouter.post("/",async(req,res,next)=>{try{res.status(201).json({campaigns:await service.create(req.user!.sub,schema.parse(req.body))});}catch(e){next(e);}});
+launchCampaignsRouter.post("/product/:productId/activate",async(req,res,next)=>{try{res.json(await service.activateNow(req.params.productId,req.user!.sub));}catch(e){next(e);}});
 launchCampaignsRouter.post("/:id/launch",async(req,res,next)=>{try{res.json({campaigns:await service.launch(req.params.id,req.user!.sub)});}catch(e){next(e);}});
 launchCampaignsRouter.post("/:id/cancel",async(req,res,next)=>{try{res.json({campaigns:await service.cancel(req.params.id,req.user!.sub)});}catch(e){next(e);}});
