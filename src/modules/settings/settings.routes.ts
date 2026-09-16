1799ca782f44ce9eaedbce6bbb9da0cd77870ad6
Zion Opaaje <zopaaje8@gmail.com>
2026-09-16 14:58:04 +0100

    Remove public maintenance settings endpoint

--- src/modules/settings/settings.routes.ts
+++ src/modules/settings/settings.routes.ts
@@ -7,14 +7,6 @@ import * as settingsService from "./settings.service";
 export const settingsRouter = Router();
 
 settingsRouter.get(
-  "/public",
-  asyncHandler(async (_req, res) => {
-    const settings = await settingsService.getPublicSettings();
-    res.json(settings);
-  })
-);
-
-settingsRouter.get(
   "/",
   requireAuth,
   requireRole("ADMIN"),
